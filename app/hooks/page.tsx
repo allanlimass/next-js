6bfcf96a804d8e0b3a4733d259b71de50fa7798c
Allan Silva <allanlimass@hotmail.com>
2025-09-04 15:54:50 -0300

    Refatoração da função de soma/subtração

--- app/hooks/page.tsx
+++ app/hooks/page.tsx
@@ -6,24 +6,11 @@ import ShowState from "@/components/ShowState"
 
 export default function UseState() {
     const [ count, setCount ] = useState<number>(0)
-
-    function sum() {
-        let c = count
-        c++
-        setCount(c)
-    }
-
-    function sub() {
-        let c = count
-        c--
-        setCount(c)
-    }
-
     return (
         <div>
             <Header/>
             <h1>useState</h1>
-            <ShowState amount={count} sum={sum} sub={sub}></ShowState>
+            <ShowState amount={count} fAmount={setCount}></ShowState>
         </div>
     )
 }
--- components/ShowState.tsx
+++ components/ShowState.tsx
@@ -1,18 +1,23 @@
 interface ShowStateProps {
     amount: number,
-    sum: any,
-    sub: any
+    fAmount: any
 }
 
 export default function ShowState(props: ShowStateProps) {
+    function operator(op: number) {
+        let c = props.amount
+        c += op
+        props.fAmount(c)
+    }
+
     return (
         <div className="flex flex-col items-center p-4 rounded-lg bg-gray-200 w-100 m-auto mt-4">
             <div className="text-6xl font-bold p-4 rounded-lg bg-white w-90 text-center">
                 {props.amount}
             </div>
             <div className="flex gap-2 mt-2">
-                <button onClick={props.sum}>+ Adicionar</button>
-                <button onClick={props.sub}>- Subtrair</button>
+                <button onClick={() => operator(1)}>+ Adicionar</button>
+                <button onClick={() => operator(-1)}>- Subtrair</button>
             </div>
         </div>
     )
